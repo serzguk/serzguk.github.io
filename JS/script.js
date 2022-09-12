44ec029c50e59977d5863472b0cddb4dce14472d
serzguk <104847173+serzguk@users.noreply.github.com>
2022-09-12 15:27:12 +0300

    Delete JS directory

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-const SvgWrapper = document.querySelector('.svg-wrapper');
-const SubMenu = document.querySelector('.sub-menu-wrapper');
-
-console.log(SvgWrapper);
-console.log(SubMenu);
-
-SvgWrapper.addEventListener('click', function(){
-    SubMenu.classList.toggle('none');
-})
